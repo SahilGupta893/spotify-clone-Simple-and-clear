804e88a9ec9812ba8608645378f7c62dd8b1985a
Sahil-gupta88 <guptasahil3284@gmail.com>
2025-01-19 01:03:45 +0530

    Updated script.js to improve functionality and fix bugs

--- script.js
+++ script.js
@@ -21,9 +21,23 @@ async function main() {
   let songs = await getSongs();
   console.log(songs);
 
-  let songUL = document.querySelector(".songlist").getElementsByTagName("ul")[0]
+  let songUL = document
+    .querySelector(".songlist")
+    .getElementsByTagName("ul")[0];
   for (const song of songs) {
-    songUL.innerHTML = songUL.innerHTML + `<li> ${song.replaceAll("%20" , " ")} </li>`
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li> 
+    <img src="music.svg" alt="">
+                            <div class="info">
+                                <div> ${song.replaceAll("%20", " ")}</div>
+                                <div>Song Artist</div>
+                            </div>
+                            <div class="playnow">
+                                <span>Play Now</span>
+                                <img src="play.svg" alt="">
+                            </div>
+   </li>`;
   }
 
   // Play the first song
@@ -31,7 +45,7 @@ async function main() {
   audio.play();
 
   audio.addEventListener("loadeddata", () => {
-    console.log(audio.duration , audio.currentSrc , audio.currentTime)
+    console.log(audio.duration, audio.currentSrc, audio.currentTime);
   });
 }
 
